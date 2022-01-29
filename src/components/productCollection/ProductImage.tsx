56cb2f937f17463d7c7be34ffbdc835dc08f3a14
Enzo <duenzo1010@gmail.com>
2022-01-29 21:17:54 +0800

    feat: router fix (6-7)

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Image, Typography } from "antd";
+import { Link } from 'react-router-dom';
+
 
 interface PropsType {
     id: string | number;
@@ -9,12 +11,12 @@ interface PropsType {
     title: string;
 }
 
-export const ProductImage: React.FC<PropsType> = ({id, size, imageSrc, price, title}) => {
+const ProductImage: React.FC<PropsType> = ({id, size, imageSrc, price, title }) => {
     return (
-      <>
+      <Link to={`detail/${id}`}>
         {size === "large" ? (
           <Image src={imageSrc} height={285} width={490} />
-        ) : (
+        ) : ( 
           <Image src={imageSrc} height={120} width={240} />
         )}
         <div>
@@ -25,6 +27,8 @@ export const ProductImage: React.FC<PropsType> = ({id, size, imageSrc, price, ti
             $ {price} 起
           </Typography.Text>
         </div>
-      </>
+      </Link>
     );
 }
+
+export { ProductImage }
