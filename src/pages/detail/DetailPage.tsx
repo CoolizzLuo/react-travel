1556b8b3c0e1f1f33b10cede62325c30b4f46f59
Enzo <duenzo1010@gmail.com>
2022-01-30 21:57:42 +0800

    feat: DetailPage- ProductComments (9-4)

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { RouteComponentProps, useParams } from 'react-router-dom';
 import axios from 'axios';
-import { Spin, Row, Col } from 'antd';
+import { Spin, Row, Col, Divider, Typography, Anchor, Menu } from 'antd';
 import styles from './DetailPage.module.css';
-import { Header, Footer, ProductIntro } from '../../components';
+import { Header, Footer, ProductIntro, ProductComments } from '../../components';
 import { DatePicker, Space } from 'antd';
+import { commentMockData } from './mockup';
 
 const { RangePicker } = DatePicker;
 
@@ -75,15 +76,52 @@ const DetailPage: React.FC<RouteComponentProps<MatchParams>> = () => {
           </Row>
         </div>
         {/* 錨點菜單 */}
-        <div className={styles['product-detail-anchor']}></div>
+        <Anchor className={styles['product-detail-anchor']}>
+          <Menu mode='horizontal'>
+            <Menu.Item key='1'>
+              <Anchor.Link href='#feature' title='產品特色'></Anchor.Link>
+            </Menu.Item>
+            <Menu.Item key='3'>
+              <Anchor.Link href='#fees' title='費用'></Anchor.Link>
+            </Menu.Item>
+            <Menu.Item key='4'>
+              <Anchor.Link href='#notes' title='預定須知'></Anchor.Link>
+            </Menu.Item>
+            <Menu.Item key='5'>
+              <Anchor.Link href='#comments' title='用戶評論'></Anchor.Link>
+            </Menu.Item>
+          </Menu>
+        </Anchor>
         {/* 產品特色 */}
-        <div id='feature' className={styles['product-detail-container']}></div>
-        {/* 费用 */}
-        <div id='fees' className={styles['product-detail-container']}></div>
+        <div id='feature' className={styles['product-detail-container']}>
+          <Divider orientation={'center'}>
+            <Typography.Title level={3}>產品特色</Typography.Title>
+          </Divider>
+          <div dangerouslySetInnerHTML={{ __html: product.features }} style={{ margin: 50 }}></div>
+        </div>
+        {/* 費用 */}
+        <div id='fees' className={styles['product-detail-container']}>
+          <Divider orientation={'center'}>
+            <Typography.Title level={3}>費用</Typography.Title>
+          </Divider>
+          <div dangerouslySetInnerHTML={{ __html: product.fees }} style={{ margin: 50 }}></div>
+        </div>
         {/* 預定須知 */}
-        <div id='notes' className={styles['product-detail-container']}></div>
+        <div id='notes' className={styles['product-detail-container']}>
+          <Divider orientation={'center'}>
+            <Typography.Title level={3}>預定須知</Typography.Title>
+          </Divider>
+          <div dangerouslySetInnerHTML={{ __html: product.notes }} style={{ margin: 50 }}></div>
+        </div>
         {/* 商品評價 */}
-        <div id='comments' className={styles['product-detail-container']}></div>
+        <div id='comments' className={styles['product-detail-container']}>
+          <Divider orientation={'center'}>
+            <Typography.Title level={3}>用戶評價</Typography.Title>
+          </Divider>
+          <div style={{ margin: 40 }}>
+            <ProductComments data={commentMockData} />
+          </div>
+        </div>
       </div>
       <Footer />
     </>
