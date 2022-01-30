c06db04d3b53e7959e8910e624226b6fb1a565d8
Enzo <duenzo1010@gmail.com>
2022-01-31 02:38:02 +0800

    feat: install @reduxjs/toolkit add productDetail/slice (9-6)

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,16 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import languageReducer from './language/languageReducer';
 import recommendProductsReducer from './recommendProducts/recommendProductsReducer';
 import thunk from 'redux-thunk';
 import { actionLog } from './middlewares/actionLog';
 import { i18nChangeLanguage } from './middlewares/i18nChangeLanguage';
+import { productDetailSlice } from './productDetail/slice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
 const rootReducer = combineReducers({
   language: languageReducer,
   recommendProducts: recommendProductsReducer,
+  productDetail: productDetailSlice.reducer,
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk, actionLog, i18nChangeLanguage));
