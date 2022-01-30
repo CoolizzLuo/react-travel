93fec65dbdaf108ee6760ccda085b153ef88a8db
Enzo <duenzo1010@gmail.com>
2022-01-30 21:21:22 +0800

    feat: DetailPage- ProductIntro (9-3)

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,16 +1,93 @@
-import React from "react";
-import { RouteComponentProps } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { RouteComponentProps, useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Spin, Row, Col } from 'antd';
+import styles from './DetailPage.module.css';
+import { Header, Footer, ProductIntro } from '../../components';
+import { DatePicker, Space } from 'antd';
 
+const { RangePicker } = DatePicker;
 
 interface MatchParams {
   touristRouteId: string;
 }
 
-const DetailPage: React.FC<RouteComponentProps<MatchParams>> = (props) => {
-//   console.log(props.history);
-//   console.log(props.location);
-//   console.log(props.match);
-  return <h1>旅遊路線詳情頁面, 路線ID: {props.match.params.touristRouteId}</h1>;
+const DetailPage: React.FC<RouteComponentProps<MatchParams>> = () => {
+  const { touristRouteId } = useParams<MatchParams>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [product, setProduct] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const { data } = await axios.get(`http://123.56.149.216:8089/api/touristRoutes/${touristRouteId}`);
+        setProduct(data);
+      } catch (e) {
+        setError('network error');
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [touristRouteId]);
+
+  if (loading) {
+    return (
+      <Spin
+        size='large'
+        style={{
+          marginTop: 200,
+          marginBottom: 200,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '100%',
+        }}
+      />
+    );
+  }
+  if (error) {
+    return <div>網站出錯{error}</div>;
+  }
+
+  return (
+    <>
+      <Header />
+      <div className={styles['page-content']}>
+        {/* 產品簡介與日期選擇 */}
+        <div className={styles['product-intro-container']}>
+          <Row>
+            <Col span={13}>
+              <ProductIntro
+                title={product.title}
+                shortDescription={product.description}
+                price={product.originalPrice}
+                coupons={product.coupons}
+                points={product.points}
+                discount={product.price}
+                rating={product.rating}
+                pictures={product.touristRoutePictures.map((p) => p.url)}
+              />
+            </Col>
+            <Col span={11}>
+              <RangePicker open style={{ marginTop: 20 }} />
+            </Col>
+          </Row>
+        </div>
+        {/* 錨點菜單 */}
+        <div className={styles['product-detail-anchor']}></div>
+        {/* 產品特色 */}
+        <div id='feature' className={styles['product-detail-container']}></div>
+        {/* 费用 */}
+        <div id='fees' className={styles['product-detail-container']}></div>
+        {/* 預定須知 */}
+        <div id='notes' className={styles['product-detail-container']}></div>
+        {/* 商品評價 */}
+        <div id='comments' className={styles['product-detail-container']}></div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
-export { DetailPage }
+export { DetailPage };
