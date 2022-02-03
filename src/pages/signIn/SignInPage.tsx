f1c137e4835315170416221ef22d3406ca1cba9f
Enzo <duenzo1010@gmail.com>
2022-02-03 22:19:28 +0800

    feat: add page SignInForm/RegisterForm

--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { UserLayout } from '../../layouts/userLayout';
+import { SignInForm } from './SignInForm';
 
 const SignInPage: React.FC = () => {
   return (
     <UserLayout>
-      <h1>登入頁面</h1>
+      <SignInForm />
     </UserLayout>
   );
 };
